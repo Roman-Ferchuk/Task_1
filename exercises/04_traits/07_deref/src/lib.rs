6958ddae09a16b771d923a7e1f889ca7eb20d919
Roman-Ferchuk <romanferchuk5@gmail.com>
2025-07-30 23:36:08 +0200

    Task 04_traits 07_deref

---
+++ exercises/04_traits/07_deref/src/lib.rs
@@ -0,0 +1,32 @@
+pub struct Ticket {
+    title: String,
+    description: String,
+    status: String,
+}
+
+impl Ticket {
+    pub fn title(&self) -> &str {
+        self.title.trim()           //.trim() returns a string slice(&str)
+    }
+
+    pub fn description(&self) -> &str {
+        self.description.trim()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_normalization() {
+        let ticket = Ticket {
+            title: "   A title ".to_string(),
+            description: " A description   ".to_string(),
+            status: "To-Do".to_string(),
+        };
+
+        assert_eq!("A title", ticket.title());
+        assert_eq!("A description", ticket.description());
+    }
+}
