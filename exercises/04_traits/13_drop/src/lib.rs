08d6ec89c641270046f75ab2711425db53313b25
Roman-Ferchuk <romanferchuk5@gmail.com>
2025-07-30 23:44:24 +0200

    Task 04_traits 13_drop

---
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -0,0 +1,41 @@
+pub struct DropBomb {
+    is_defused: bool,
+}
+
+impl DropBomb {
+    fn new() -> Self {
+        DropBomb { is_defused: false }
+    }
+
+    fn defuse(&mut self) {
+        self.is_defused = true;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.is_defused {
+            panic!("");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn test_drop_bomb() {
+        let bomb = DropBomb::new();
+        // The bomb should panic when dropped
+    }
+
+    #[test]
+    fn test_defused_drop_bomb() {
+        let mut bomb = DropBomb::new();
+        bomb.defuse();
+        // The bomb should not panic when dropped
+        // since it has been defused
+    }
+}
